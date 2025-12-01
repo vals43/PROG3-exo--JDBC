7d20eb7b5c96457ccb6d0e01187586d8ddee9e0a
Vals43 <hei.teddy.3@gmail.com>
2025-12-01 13:22:52 +0300

    Setup dependancies

---
+++ src/main/java/prog3/exojdbc/ExoJdbcApplication.java
@@ -0,0 +1,13 @@
+package prog3.exojdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExoJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExoJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/prog3/exojdbc/ExoJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package prog3.exojdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExoJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
