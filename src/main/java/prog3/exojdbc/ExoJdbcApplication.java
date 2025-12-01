0ff7a1dbf9c96f463b77e120b1c991b105df22ae
Vals43 <hei.teddy.3@gmail.com>
2025-12-01 13:44:00 +0300

    add controller

--- src/main/java/prog3/exojdbc/ExoJdbcApplication.java
+++ src/main/java/prog3/exojdbc/ExoJdbcApplication.java
@@ -1,13 +1,21 @@
 package prog3.exojdbc;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExoJdbcApplication {
-
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+        dotenv.entries().forEach(e ->
+                System.setProperty(e.getKey(), e.getValue())
+        );
+
         SpringApplication.run(ExoJdbcApplication.class, args);
     }
-
 }
